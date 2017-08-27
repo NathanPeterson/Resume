4dbd6c17e23be8fa23c80676f2ac982082b68e2b
Nathan Peterson <nalexpeterson@gmail.com>
2017-08-26 20:40:16 -0400

    Init commit

---
+++ resume.js
@@ -0,0 +1,23 @@
+var currentTab = 'about';
+
+$(document).ready(function(){
+  showTabContent(currentTab);
+
+  $('li').click(function() {
+    hideTabContent(currentTab);
+    var idName = this.id.split('-').pop();
+    showTabContent(idName);
+  })
+})
+
+
+function hideTabContent(previousTab){
+  $('#tab-' + previousTab).removeClass('active');
+  $('#' + previousTab).addClass('hidden');
+}
+
+function showTabContent(contentId){
+  $('#tab-' + contentId).addClass('active');
+  $('#' + contentId).removeClass('hidden');
+  currentTab = contentId;
+}
